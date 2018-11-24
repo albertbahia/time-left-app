cc913cc8869f5f38cec5aeeb9bc28cd7fe292ecb
albertbahia <albert.bahia1@gmail.com>
2018-11-23 21:55:08 -0500

    more time stuff

--- main.js
+++ main.js
@@ -3,6 +3,19 @@ $( document ).ready(function() {
    const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
    ];
+   
+   // -----------------------
+   // Meet Time Variables
+   // -----------------------
+   let meetDate = new Date('November 30, 2018 22:00:00');
+   let meetDd = meetDate.getDay();
+   let meetMm = meetDate.getMonth();
+   let meetHh = meetDate.getHours();
+   let meetMins = meetDate.getMinutes();
+   let meetSs = meetDate.getSeconds();
+   // -----------------------
+
+   $("#meet-time-value").text(meetDate);
 
    function startTime() {
       // -----------------------
@@ -20,20 +33,7 @@ $( document ).ready(function() {
       }
       // -----------------------
 
-      // -----------------------
-      // Meet Time Variables
-      // -----------------------
-      let meetDate = new Date('November 30, 2018 22:00:00');
-      let meetDd = meetDate.getDay();
-      let meetMm = meetDate.getMonth();
-      let meetHh = meetDate.getHours();
-      let meetMins = meetDate.getMinutes();
-      let meetSs = meetDate.getSeconds();
-      // -----------------------
-
-
-      
-      // console.log("Date Value: " + dd);
+      console.log("Date Value: " + dd);
       // console.log("Month Value: " + mm);
       // console.log("Hours Value: " + hh );
       // console.log("Year Value: " + YYYY);
@@ -41,14 +41,16 @@ $( document ).ready(function() {
       // console.log("Seconds Value: " + ss);
       // console.log(today);
       
-      console.log(meetDate);
+      // console.log(meetDate);
       console.log(meetDd);
-      console.log(meetMm);
-      console.log(meetHh);
-      console.log(meetMins);
-      console.log(meetSs);
+      // console.log(meetMm);
+      // console.log(meetHh);
+      // console.log(meetMins);
+      // console.log(meetSs);
       
-      $("#time-left-value").text(monthNames[mm] +  " " + dd + ", " + YYYY + ": " +  hh + ":" + mins + ":" + ss);
+      $("#current-time-value").text(monthNames[mm] +  " " + dd + ", " + YYYY + ": " +  hh + ":" + mins + ":" + ss);
+
+      $("#time-left-value").text((dd-meetDd) + "Days")
 
       setTimeout(startTime, 1000);
 
