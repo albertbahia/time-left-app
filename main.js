91872b36b6258a2be8d6dc7a0bb2c6de73521290
albertbahia <albert.bahia1@gmail.com>
2018-11-25 16:54:42 -0500

    added comments for better organization

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 $( document ).ready(function() {
 
+   // -----------------------
+   // Constants
+   // -----------------------
    const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
    ];
 
    const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+   // -----------------------
    
    // -----------------------
    // Meet Time Variables
