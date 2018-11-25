a98613e2b639d5d46ba77a8709fb01620008290f
albertbahia <albert.bahia1@gmail.com>
2018-11-25 16:06:28 -0500

    added array of day names

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ $( document ).ready(function() {
    const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
    ];
+
+   const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
    
    // -----------------------
    // Meet Time Variables
