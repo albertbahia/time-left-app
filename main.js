5e204cdc9dff39ddd521ac1f660c23169b1b2a1c
albertbahia <albert.bahia1@gmail.com>
2019-05-07 15:28:52 -0400

    updated next time to meet timetstamp

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ $( document ).ready(function() {
    // -----------------------
    // Meet Time Variables
    // -----------------------
-   let meetDate = new Date('May 3, 2019 23:30:00');
+   let meetDate = new Date('May 10, 2019 23:30:00');
    let meetDd = meetDate.getDate();
    let meetDdName = meetDate.getDay();
    let meetMm = meetDate.getMonth();
