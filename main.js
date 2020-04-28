cf7279dddc103caf3be0eeb5931aaf44a1f65fe8
albertbahia <albert.bahia1@gmail.com>
2020-04-28 08:37:37 -0400

    updated next time to meet to this Sunday

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ $( document ).ready(function() {
    // -----------------------
    // Meet Time Variables
    // -----------------------
-   let meetDate = new Date('April 26, 2020 15:30:00');
+   let meetDate = new Date('May 3, 2020 15:30:00');
    let meetDd = meetDate.getDate();
    let meetDdName = meetDate.getDay();
    let meetMm = meetDate.getMonth();
